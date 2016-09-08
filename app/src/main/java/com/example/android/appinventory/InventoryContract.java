391204793211d966e051c429358e7b4b2ea9265e
blkh20 <joshua_brandon_garcia@yahoo.com>
2016-09-08 10:30:20 -0500

    Update

--- app/src/main/java/com/example/android/appinventory/InventoryContract.java
+++ app/src/main/java/com/example/android/appinventory/InventoryContract.java
@@ -6,10 +6,10 @@ import android.provider.BaseColumns;
 public class InventoryContract {
     public static abstract class ProductEntry implements BaseColumns {
         public static final String TABLE_NAME = "Products";
-        public static final String COLUMN_NAME_PRODC_NAME = "ProductName";
+        public static final String COLUMN_NAME_PRODUCT_NAME = "ProductName";
         public static final String COLUMN_NAME_QUANTITY = "Quantity";
         public static final String COLUMN_NAME_PRICE = "Price";
         public static final String COLUMN_NAME_IMAGE = "Image";
-        public static final String COLUMN_NAME_SOlD = "Sold";
+        public static final String COLUMN_NAME_SOLD = "Sold";
     }
 }
--- app/src/main/java/com/example/android/appinventory/InventoryDbHelper.java
+++ app/src/main/java/com/example/android/appinventory/InventoryDbHelper.java
@@ -5,20 +5,16 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class InventoryDbHelper extends SQLiteOpenHelper {
-    private final static String TEXT_TYPE = " TEXT";
-    private static final String INT_TYPE = " INTEGER";
-    private static final String REAL_TYPE = " REAL";
-    private static final String COMMA_SEP = ",";
-    private static final int DATABASE_VERSION = 4;
+    private static final int DATABASE_VERSION = 1;
     private static final String DATABASE_NAME = "Products.db";
     private static final String SQL_CREATE_ENTRIES = "CREATE TABLE "
             + InventoryContract.ProductEntry.TABLE_NAME + " ("
-            + InventoryContract.ProductEntry._ID + " INTEGER PRIMARY KEY" + COMMA_SEP
-            + InventoryContract.ProductEntry.COLUMN_NAME_PRODC_NAME + TEXT_TYPE + COMMA_SEP
-            + InventoryContract.ProductEntry.COLUMN_NAME_QUANTITY + INT_TYPE + COMMA_SEP
-            + InventoryContract.ProductEntry.COLUMN_NAME_PRICE + REAL_TYPE + COMMA_SEP
-            + InventoryContract.ProductEntry.COLUMN_NAME_IMAGE + TEXT_TYPE + COMMA_SEP
-            + InventoryContract.ProductEntry.COLUMN_NAME_SOlD + INT_TYPE + ")";
+            + InventoryContract.ProductEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + InventoryContract.ProductEntry.COLUMN_NAME_PRODUCT_NAME + " TEXT NOT NULL,"
+            + InventoryContract.ProductEntry.COLUMN_NAME_QUANTITY + "INTEGER NOT NULL,"
+            + InventoryContract.ProductEntry.COLUMN_NAME_PRICE + "INTEGER NOT NULL,"
+            + InventoryContract.ProductEntry.COLUMN_NAME_IMAGE + "TEXT NOT NULL,"
+            + InventoryContract.ProductEntry.COLUMN_NAME_SOLD + "TEXT NOT NULL);";
 
     private static final String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS "
             + InventoryContract.ProductEntry.TABLE_NAME;
