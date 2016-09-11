56343411aea55abcc153794b3cb27d8937688bb4
blkh20 <joshua_brandon_garcia@yahoo.com>
2016-09-10 23:36:58 -0500

    Update

--- app/src/main/java/com/example/android/appinventory/InventoryDbHelper.java
+++ app/src/main/java/com/example/android/appinventory/InventoryDbHelper.java
@@ -7,21 +7,20 @@ import android.util.Log;
 
 public class InventoryDbHelper extends SQLiteOpenHelper {
     public static final String LOG_TAG = InventoryDbHelper.class.getSimpleName();
-    private static final String DATABASE_NAME = " Products.db ";
-    private static final int DATABASE_VERSION = 4;
-    private static final String SQL_DELETE_ENTRIES = " DROP TABLE IF EXISTS "
-            + InventoryContract.ProductEntry.TABLE_NAME;
+
+    private static final int DATABASE_VERSION = 1;
+
+    static final String DATABASE_NAME = "product.db";
 
     public InventoryDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
-
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
 
         Log.d(LOG_TAG, "onCreate method called");
         String SQL_CREATE_TABLE = " CREATE TABLE "
-                + InventoryContract.ProductEntry.TABLE_NAME + " ("
+                + InventoryContract.ProductEntry.TABLE_NAME + InventoryContract.ProductEntry.TABLE_NAME + " ("
                 + InventoryContract.ProductEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
                 + InventoryContract.ProductEntry.COLUMN_NAME_PRODUCT_NAME + " TEXT NOT NULL, "
                 + InventoryContract.ProductEntry.COLUMN_NAME_QUANTITY + " INTEGER NOT NULL, "
@@ -30,17 +29,10 @@ public class InventoryDbHelper extends SQLiteOpenHelper {
                 + InventoryContract.ProductEntry.COLUMN_NAME_SOLD + " TEXT NOT NULL);";
         sqLiteDatabase.execSQL(SQL_CREATE_TABLE);
     }
-    // Method to delete the db
-    public void deleteDatabase(Context context, String dbName) {
-        context.deleteDatabase(dbName);
-        Log.d(LOG_TAG, "deleteDatabase method called");
-    }
-
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-        sqLiteDatabase.execSQL(SQL_DELETE_ENTRIES);
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+        sqLiteDatabase.execSQL("drop table if exists " + InventoryContract.ProductEntry.TABLE_NAME);
         onCreate(sqLiteDatabase);
-        Log.d(LOG_TAG, "onUpgrade method called");
     }
 }
 
