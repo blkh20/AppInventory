6a3defccc868b04fa8a756f015ea27fdb41aa190
blkh20 <joshua_brandon_garcia@yahoo.com>
2016-09-12 12:17:07 -0500

    Update

--- app/src/main/java/com/example/android/appinventory/Data/ProductContract.java
+++ app/src/main/java/com/example/android/appinventory/Data/ProductContract.java
@@ -5,8 +5,7 @@ import android.provider.BaseColumns;
 
 
 public class ProductContract {
-    // Database Name
-    public static final String DATABASE_NAME = "ProductInventory";
+
     // Database Version
     public static final int DATABASE_VERSION = 1;
 
